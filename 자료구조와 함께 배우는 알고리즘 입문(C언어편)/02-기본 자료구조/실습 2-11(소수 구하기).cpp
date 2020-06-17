f5b06524f62b8e2558ca9cc333bc0f935eea377c
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-17 16:16:33 +0900

    Create 실습 2-11(소수 구하기).cpp

---
+++ 자료구조와 함께 배우는 알고리즘 입문(C언어편)/02-기본 자료구조/실습 2-11(소수 구하기).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+//N의 제곱근 이하의 어떤 소수로도 나누어떨어지지 않는다면
+//N은 소수라고 판단할 수 있음.
+
+bool ReturnBool(int n) {
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) return 0;
+	}
+	return 1;
+}
+
+int main() {
+	//n까지의 소수 구하기
+	int n; cin >> n;
+	for (int i = 2; i <= n; i++) {
+		if (ReturnBool(i)) cout << i << endl;
+	}
+}
