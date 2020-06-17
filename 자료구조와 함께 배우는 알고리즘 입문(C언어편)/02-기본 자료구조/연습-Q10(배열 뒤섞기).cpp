9d58941eb09c81000805f0c56ec77258f843ab49
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-17 16:16:36 +0900

    Create 연습-Q10(배열 뒤섞기).cpp

---
+++ 자료구조와 함께 배우는 알고리즘 입문(C언어편)/02-기본 자료구조/연습-Q10(배열 뒤섞기).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <time.h>
+#define swap(type, x, y) do{type t = x; x=y; y=t;} while(0)
+using namespace std;
+
+//배열의 모든 요소를 뒤섞는 shuffle 함수
+
+void Shuffle(int a[], int n) {
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		int j = rand() % n;
+		if (i != j) swap(int, a[i], a[j]);
+	}
+}
+
+int main() {
+	const int num = 5;
+	int arr[num] = { 1,2,3,4,5 };
+	Shuffle(arr, num);
+	for (int i = 0; i < num; i++) {
+		cout << arr[i] << " ";
+	}
+}
